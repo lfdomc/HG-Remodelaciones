5a6a35004ae29fbfd58c5bc1480755a24f96500e
Luis Morua <lfdomc@gmail.com>
2025-08-23 01:46:39 -0600

    7

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,26 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: '/',
+      },
+      {
+        userAgent: 'AdsBot-Google',
+        allow: '/',
+      }
+    ],
     sitemap: 'https://hgremodelaciones.com/sitemap.xml',
   }
 }
